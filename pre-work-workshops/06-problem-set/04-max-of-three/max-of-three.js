8cb07923ca2548ce9d7efe380f44032ceb35463a
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-17 17:54:29 -0500

    It is the largest.

--- pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
+++ pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
@@ -1,1 +1,8 @@
 // YOUR CODE BELOW
+
+function maxOfThree(one, two, three) {
+    let max = null;
+    if (one > two && one > three) return one;
+    if (two > one && two > three) return two;
+    if (three > one && three > two) return three;
+}
