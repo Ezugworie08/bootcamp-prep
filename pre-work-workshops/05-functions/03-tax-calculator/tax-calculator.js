b317ff7886648cdbbdf4eff9b460a50bf6a9e6f2
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-17 14:15:11 -0500

    Tax calculator works!

--- pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+
+function taxCalculator(price, state) {
+    let 
+        tax = null,
+        cost = null;
+    if (state === 'NY') {
+        tax = (4/100);
+        cost = calculatePrice(price, tax);
+    } else if (state === 'NJ') {
+        tax = (6.625/100);
+        cost = calculatePrice(price, tax);
+    }
+    // return Number(cost.toFixed(2));
+    return cost;
+
+    function calculatePrice(amt, taxVal) {
+        return (amt + (amt * taxVal));
+    }
+}
