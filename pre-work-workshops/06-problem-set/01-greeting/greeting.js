7e20872712f2aa57123e493c3155ecc1498d9fc8
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-17 16:44:46 -0500

    I can greet now

--- pre-work-workshops/06-problem-set/01-greeting/greeting.js
+++ pre-work-workshops/06-problem-set/01-greeting/greeting.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+
+function greeting(name) {
+    let greet = null;
+    if (name && typeof name === 'string') greet = 'Hello ' + name + '!';
+    else greet = 'Hello!';
+
+    return greet;
+}
