cafa40768d99bfe9d8682cecd9272b529d228338
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-17 13:05:46 -0500

    Default greeting works, yay!

--- pre-work-workshops/05-functions/02-default-greet/default-greet.js
+++ pre-work-workshops/05-functions/02-default-greet/default-greet.js
@@ -1,1 +1,7 @@
 // YOUR CODE BELOW
+function defaultGreet(first, last) {
+    first = (first) ? String(first) : 'John';
+    last = (last) ? String(last) : 'Doe';
+
+    return ("Hi " + first + " " + last + "!");
+}
