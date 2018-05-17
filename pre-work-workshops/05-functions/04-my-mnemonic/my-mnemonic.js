e13784c5e2b6fac7254f045f2fd16b5aaccf83b6
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-17 16:35:26 -0500

    Passed all tests

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+
+function myMnemonic(one, two, three, four) {
+    let mnemonic = '';
+    if (one && typeof one === 'string') { mnemonic += one[0]; }
+    if (two && typeof two === 'string') { mnemonic += two[0]; }
+    if (three && typeof three === 'string') { mnemonic += three[0]; }
+    if (four && typeof four === 'string') { mnemonic += four[0]; }
+    return mnemonic;
+}
