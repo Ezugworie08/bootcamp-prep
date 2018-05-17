de04b900e729ab935b951d7b021f0c1e8861d36e
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-17 18:01:23 -0500

    Every which way na way finally.

--- pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
+++ pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+
+function everyWhichWay(one, two, three) {
+    let out = null;
+
+    if ((one + two) === three) out = 'sum';
+    if ((one - two) === three) out = 'difference';
+    if ((one * two) === three) out = 'product';
+    if ((one / two) === three) out = 'fraction';
+
+    return out;
+}
