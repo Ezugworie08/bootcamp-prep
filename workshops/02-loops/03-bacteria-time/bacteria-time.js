a9ff6480db18551d74d00ecb37d0010601187495
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-18 13:24:34 -0500

    Bacteria time solved.

--- workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+
+function bacteriaTime(init, final) {
+    const factor = 20
+    let counter = 0;
+
+    if (init > final) return 'targetNum must be larger than currentNum';
+
+    while(final > init) {
+        init *= 2;
+        counter += 1;
+    }
+
+    return (counter * factor);
+}
