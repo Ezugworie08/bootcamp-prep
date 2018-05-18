4c4215be0ace48773c79f948184efe7563404fbe
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-18 12:54:33 -0500

    Crazy caps is sorted.

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+
+function crazyCaps(str) {
+    str = str.toLowerCase();
+    let crazy = str[0];
+    const strLen = str.length;
+
+    for (let i = 1; i < strLen; i++) {
+        if ((i % 2) !== 0) {
+            crazy += str[i].toUpperCase();
+        } else {
+            crazy += str[i];
+        }
+    }
+
+    return crazy;
+}
