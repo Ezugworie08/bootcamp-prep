323506b8f59942251944cea759a851545074095c
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-18 16:02:12 -0500

    Let there be exponentiate

--- workshops/02-loops/04-exponentiate/exponentiate.js
+++ workshops/02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+
+function exponentiate (base, power) {
+    let exponent = 1;
+
+    if (power === 0) return exponent;
+    else {
+        for (let i = 1; i <= power; i++) {
+            exponent *= base;
+        }
+    }
+    return exponent;
+}
