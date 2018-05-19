a0dfa829b24ba70232e26b0e545056a4b4bec49d
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-19 11:12:56 -0500

    Slicing strings easy peasy

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+
+function mySlice(str, start, stop) {
+    let slice = '';
+
+    str = (str) ? String(str) : '';
+    const strLen = str.length;
+    start = isNaN(Number(start)) ? 0 : Number(start);
+    stop = isNaN(Number(stop)) ? strLen : Number(stop);
+
+    if (start < 0) start = 0;
+    if (start > strLen) start = strLen;
+    if (stop < 0) stop = (strLen + stop);
+    if (stop > strLen) stop = strLen;
+    if (start === stop) return slice;
+    if (start > stop) return slice;
+
+    for (let i = start; i < stop; i++) {
+        slice += str[i];
+    }
+
+    return slice;
+}
