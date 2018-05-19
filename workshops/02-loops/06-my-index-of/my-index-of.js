129d8969a3a7b4166e64be871259ab7f53402b53
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-19 12:24:16 -0500

    indexOf for strings, sorted.

--- workshops/02-loops/06-my-index-of/my-index-of.js
+++ workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,27 @@
 // YOUR CODE BELOW
+
+function myIndexOf(source, search, start) {
+    let idx = -1;
+    source = (source) ? String(source) : '';
+    let sourceLen = source.length;
+    search = (search) ? String(search) : '';
+    let searchLen = search.length;
+    start = isNaN(Number(start)) ? 0 : Number(start);
+    let endIdx = sourceLen - (searchLen - 1);
+
+    if (source === search && start === 0) { idx = 0; return idx; }
+    if (search === '' || source  === '') return sourceLen;
+
+    for (let i = start; i < endIdx; i += 1) {
+        let tempStr = '';
+        for (let j = 0; j < searchLen; j += 1) {
+            if (source[i + j] === search[j]) {
+                tempStr += source[i + j];
+            } else {
+                break;
+            }
+        }
+       if (tempStr === search) { idx = i; return idx; }
+    }
+    return idx;
+}
