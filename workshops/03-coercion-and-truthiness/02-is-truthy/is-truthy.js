3249c5664d5f20f75b09914f8184b1e3b373c56e
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-19 18:06:38 -0500

    It is falsey bro

--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+
+function isTruthy(value) {
+    let type = typeof value;
+    let msg = '';
+
+    if (value) {
+        msg = true;
+    } else if (type === 'undefined') {
+        msg = 'undefined is falsey';
+    } else if (type === 'number') {
+        msg = 'The ' + type + ' 0 is falsey (the only falsey number)';
+    } else if (type === 'string') {
+        msg = 'The empty ' + type + ' is falsey (the only falsey string)';
+    } else if (type === 'boolean') {
+        msg = 'The ' + type + ' value false is falsey';
+    } else if (value === null) {
+        msg = 'The null value is falsey';
+    }
+    
+    return msg;
+}
