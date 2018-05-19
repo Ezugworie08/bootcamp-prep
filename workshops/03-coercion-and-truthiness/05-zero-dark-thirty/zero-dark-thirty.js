3fe2eaa61414e1f46e3ebc659f8908cca69c8352
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-19 18:53:05 -0500

    Zero-Dark-Thirty removes all zero digits in number

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+
+function zeroDarkThirty(value) {
+    value = Number(value)
+    const strValue = String(value);
+    const strLen = strValue.length;
+    let newValue = '';
+    if (value) {
+        for (let i = 0; i < strLen; i++) {
+            if (strValue[i] !== '0') {
+                newValue += strValue[i];
+            }
+        }
+        return Number(newValue);
+    } else {
+        return NaN;
+    }
+}
