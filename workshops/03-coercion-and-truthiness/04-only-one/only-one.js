f7e80a30ea91c73180941bde2e69046d33e827a6
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-19 18:27:20 -0500

    All tests passed

--- workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+// I didn't get this one right this time.
+
+function onlyOne(one, two, three) {
+    return (
+        (!!one && !two && !three) ||
+        (!one && !!two && !three) ||
+        (!one && !two && !!three)
+        // !!one + !!two + !!three === 1;
+    );
+}
