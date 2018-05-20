079c9828252f8f5a8ef7a2fd3647ca16e52327a3
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-20 03:21:50 -0500

    The unholy matrimony of evens and odds

--- workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,8 @@
 // YOUR CODE BELOW
+
+function evenAndOdd(array) {
+    return [
+        array.filter(function(elem){ if(elem % 2 === 0) return elem}),
+        array.filter(function(elem){ if(elem % 2 !== 0) return elem})
+    ];
+}
