a70843b5aa65c0f6459ec65239e1b8d17ec1e51c
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-20 18:44:15 -0500

    Joining array elements is easy

--- workshops/06-arrays-ii/06-my-join/my-join.js
+++ workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+
+function myJoin(array, separator) {
+    separator = (separator === undefined) ? ',' : separator;
+    let joined = (array[0]) ? array[0] : ''; 
+    let len = array.length;
+
+    for (let i = 1; i < len; i++) {
+        if (array[i] === null || array[i] === undefined) {
+            joined += separator + '';
+        } else {
+            joined += separator + array[i] + '';
+        }
+    }
+    return joined;
+}
