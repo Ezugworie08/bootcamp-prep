f0467273aa3b06acd18a9e073a2d72448561d53f
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-20 04:56:47 -0500

    Specify columns and rows to make grid

--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+
+function makeGrid(cols, rows) {
+    let
+        grid = [],
+        rowArr = [];
+    
+    for (let i = 1; i <=cols; i++){
+        rowArr.push(i);
+    }
+
+    for (let j = 0; j < rows; j++) {
+        grid.push(rowArr);
+    }
+
+    return grid;
+}
