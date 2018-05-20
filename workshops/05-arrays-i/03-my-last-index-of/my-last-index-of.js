eff359e15c997b53e0c46a6c40c965fa8e2e84d8
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-20 02:56:08 -0500

    My last index of has been found

--- workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
+++ workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+
+function myLastIndexOf(array, search, startIdx) {
+    const arrLen = array.length;
+    startIdx = isNaN(Number(startIdx)) ? arrLen : Number(startIdx);
+
+    for (let i = startIdx; i >=0; i--) {
+        if (array[i] === search) {
+            return i;
+        }
+    }
+    return -1;
+}
