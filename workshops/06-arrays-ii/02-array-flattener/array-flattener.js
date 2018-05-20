759e8244a7a5041a0de79c80502ec6bbe1021ba4
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-20 03:34:19 -0500

    ... and Arrays were flattened.

--- workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+
+function arrayFlattener(array) {
+    const arrLen = array.length;
+    let flattened = [];
+
+    for (let i = 0; i < arrLen; i++) {
+        if (Array.isArray(array[i])) {
+            flattened = flattened.concat(arrayFlattener(array[i]));
+        } else {
+            flattened.push(array[i]);
+        }
+    }
+    return flattened;
+}
