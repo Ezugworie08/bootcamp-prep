0b4183d08fd59736e16119297fbc6166376f507f
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-20 11:18:19 -0500

    Selectivley remove columns from grid

--- workshops/06-arrays-ii/05-remove-columns/remove-columns.js
+++ workshops/06-arrays-ii/05-remove-columns/remove-columns.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+
+function removeColumns(grid, cols) {
+    const len = grid.length;
+
+    for (let i = 0; i < len; i++) {
+        for (j = 0; j < cols; j++) {
+            grid[i].pop();
+        }
+    }
+    return grid;
+}
