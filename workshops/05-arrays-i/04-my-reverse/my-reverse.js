ed775d6e2e3b18035463d7381bef6c5fd0017f71
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-20 03:04:29 -0500

    Go forth and reverse arrays

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+
+function myReverse(array) {
+    const startPos = array.length - 1;
+    let reversed = [];
+
+    for (let i = startPos; i >= 0; i--) {
+        reversed.push(array[i]);
+    }
+
+    return reversed;
+}
