04a9232eb1ae6169631e4d18a6cc371184e65596
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-20 04:09:56 -0500

    Zoo inventory is up to date.

--- workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+
+function zooInventory(zoo) {
+    let out = [];
+    const len = zoo.length;
+
+    for (let i = 0; i < len; i++) {
+        let animal = zoo[i];
+        let msg = animal[0] + ' the ' +  animal[1][0] + ' is ' + animal[1][1] + '.';
+        out.push(msg);
+    }
+    return out;
+}
