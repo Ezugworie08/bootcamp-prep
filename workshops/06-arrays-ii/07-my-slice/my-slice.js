cccd123aefd14132bdd26e2f400c517392c29194
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-20 21:25:39 -0500

    Slicing arrays like a boss

--- workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+
+function mySlice(array, startIdx, endIdx) {
+    const len = array.length;
+    let 
+        sliced = [],
+        startPos = isNaN(Number(startIdx)) ? 0 : Number(startIdx),
+        endPos = isNaN(Number(endIdx)) ? len : Number(endIdx);
+
+    if (startPos < 0) startPos = (len + startPos);
+    if (startPos > len) startPos = len;
+    if (endPos < 0) endPos = (len + endPos);
+    if (endPos > len) endPos = len;
+    if (startPos === endPos || startPos > endPos) return sliced;
+
+    for (let i = startPos; i < endPos; i++) {
+        sliced.push(array[i]);
+    }
+
+    return sliced;
+}
