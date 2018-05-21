28770f8ce184464f34aec4f9890db3f4a8e31d33
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-21 01:21:45 -0500

    Frequency is analysed

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+
+function frequencyAnalysis(str) {
+    const len = str.length;
+    let frequency = {};
+
+    for (let i = 0; i < len; i++) {
+        if (!frequency.hasOwnProperty(str[i])) {
+            frequency[str[i]] = 1;
+        } else{
+            frequency[str[i]] += 1;
+        }
+    }
+
+    return frequency;
+}
