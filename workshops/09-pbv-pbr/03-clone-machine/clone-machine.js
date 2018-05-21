46fb6a1d84e9642470df62dd8eb48f7e3d3e7aa3
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-21 04:07:37 -0500

    Setup the infrastructre for cloning

--- workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
+++ workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+
+function cloneMachine(animal) {
+    clone = {
+        name: animal.name + 'Clone',
+        species: animal.species,
+        offspring: []
+    };
+    animal.offspring.push(clone.name);
+    return clone;
+}
