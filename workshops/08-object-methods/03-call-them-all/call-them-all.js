49de7e6e5e4d70938cf047f02221abd7385142c4
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-21 02:55:00 -0500

    Called them all.

--- workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+
+function callThemAll(obj, value) {
+    let returned = [];
+
+    for (let key in obj) {
+        if (typeof obj[key] === 'function') {
+            returned.push(obj[key](value));
+        }
+    }
+    return returned;
+}
