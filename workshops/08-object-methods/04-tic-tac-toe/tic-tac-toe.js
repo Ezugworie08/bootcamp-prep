ff59fc04fff1ad34282e3a7dd0e6dca1fd3bae79
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-21 03:18:05 -0500

    Tic Tac Toe

--- workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+
+const ticTacToe = {
+    board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ],
+
+    move: function(char, row, col) {
+        if (this.board[row][col] === null) {
+            this.board[row][col] = char;
+        }
+        return this.board;
+    },
+
+    clear: function(row, col) {
+        this.board = [
+            [null, null, null],
+            [null, null, null],
+            [null, null, null]
+        ]
+        return this.board;
+    }
+};
