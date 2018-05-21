ba0c8621425f9c191d268ff2dce6e3ea90496e17
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-20 22:46:21 -0500

    Arrays can now be safely rotated

--- workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+
+function rotateArray(array, spin) {
+    const len = array.length;
+    let rotated = array.slice();
+    spin = isNaN(Number(spin)) ? 0 : Number(spin);
+    spins = Math.abs(spin);
+    // spins = Math.abs(spins) % len;
+
+
+    if (spin === 0) return rotated;
+
+    for (let i = 0; i < spins; i++)  {
+        if (spin > 0) {
+            rotated.unshift(rotated.pop());
+        } else {
+            rotated.push(rotated.shift());
+        }
+    }
+
+    return rotated;
+}
