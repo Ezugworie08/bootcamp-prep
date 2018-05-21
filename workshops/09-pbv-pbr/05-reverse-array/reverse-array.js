f5e40c805c73f34870ec11edbff13fafe2abc119
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-21 05:06:15 -0500

    Reversing like a boss

--- workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
+++ workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+
+function reverseArray(array) {
+    const lastIdx = array.length - 1;
+    const revLen = Math.floor(array.length/2);
+
+    for (let i = 0; i < revLen; i++) {
+        let 
+            j = lastIdx - i,
+            tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp; 
+    }
+    
+    return array;
+}
