21d1a7be195282d832c248c517e0d1e20e99324e
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-21 05:17:40 -0500

    Deep copying at the deep blue sea

--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+
+function deeperCopy(array) {
+    let copy = [];
+    const len = array.length;
+
+    for (let i = 0; i < len; i++) {
+        let currentElem = array[i];
+        if(Array.isArray(currentElem)) {
+           let innerArray = [];
+           for (let j = 0; j < currentElem.length; j++) {
+               innerArray.push(currentElem[j]);
+           }
+           copy.push(innerArray);
+        } else {
+            copy.push(currentElem);
+        }
+    }
+
+    return copy;
+}
