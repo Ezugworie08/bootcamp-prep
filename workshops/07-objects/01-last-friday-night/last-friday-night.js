3a978edfa674d88a62c282b7e76195302bfb9f80
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-21 00:10:07 -0500

    last friday night was a blast

--- workshops/07-objects/01-last-friday-night/last-friday-night.js
+++ workshops/07-objects/01-last-friday-night/last-friday-night.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+
+// function lastFridayNight(expenses) {
+//     return (expenses.reduce(function(acc, elem){
+//         return (acc + elem.amount);
+//     }, 0));
+// }
+
+function lastFridayNight(expenses) {
+    let sum = 0;
+    for (let i = 0; i < expenses.length; i++) {
+        sum += expenses[i].amount;
+    }
+    return sum;
+}
