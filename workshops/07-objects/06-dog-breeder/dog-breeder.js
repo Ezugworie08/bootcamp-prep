ca0259b957b05cf2449797eca03e5edb23bdc3bd
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-21 01:46:23 -0500

    This dog breeder can be a pesky problem to solve.

--- workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+
+function dogBreeder(name, age) {
+    if (
+        (name === undefined) || 
+        (String(name).trim() === '') ||
+        (typeof name !== 'string')
+    ) {
+        age = name;
+        name = 'Steve';
+    }
+    age = isNaN(Number(age)) ? 0 : Number(age);
+    return {name:name, age:age}
+}
