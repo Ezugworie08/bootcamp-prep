22c7cc488af3da77935f4bf9ad97244f74040800
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-21 04:54:17 -0500

    mySplice exactly to test specification

--- workshops/09-pbv-pbr/04-my-splice/my-splice.js
+++ workshops/09-pbv-pbr/04-my-splice/my-splice.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+
+function mySplice(array, start, delCount, toAdd) {
+    const len = array.length;
+    let deleted = [], beforeStart = [];
+    
+    for (let i = 0; i < start; i++) {
+        beforeStart.push(array.shift());
+    }
+
+    for (let k = 0; k < delCount; k += 1){
+        deleted.push(array.shift());
+    }
+
+    // Insert the `toAdd` element to array.
+    if (toAdd) array.unshift(toAdd);
+
+    for (let j = beforeStart.length; j > 0; j -= 1){
+        array.unshift(beforeStart.pop())
+    }
+
+    return deleted;
+
+}
