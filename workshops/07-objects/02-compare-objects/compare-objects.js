197b11413bc545ee29c24119171e8ebfda187e5c
Ezugworie08 <ezugworie.ikechukwu@live.com>
2018-05-21 00:37:07 -0500

    Object.keys came in handy ...

--- workshops/07-objects/02-compare-objects/compare-objects.js
+++ workshops/07-objects/02-compare-objects/compare-objects.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+
+function compareObjects(one, two) {
+    if (Object.keys(one).length !== Object.keys(two).length) {
+        return false;
+    }
+    for (let key in one) {
+        if (!two.hasOwnProperty(key)) return false;
+        if (one[key] !== two[key]) return false;
+    }
+    return true;
+}
